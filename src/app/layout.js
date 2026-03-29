3399a84096402549bba89da45ea3558446957262
Mihail Bass <mbass@m-m7q2gjc30l.home>
2026-03-29 00:38:03 -0400

    Massive CSS Overhaul for Native Mobile Responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,12 @@ export const metadata = {
   description: 'Nokia Beach Volleyball League Management System',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
